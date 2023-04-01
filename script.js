03cd712d04a93fdff62175e28df4596f0998a41a
Alimardon Toshpo'latov <alimardoncoder001@gmail.com>
2023-04-01 16:18:00 +0500

    loops started

---
+++ script.js
@@ -0,0 +1,38 @@
+// problem_92
+// let n = 5, s = 1;
+// for (let i = 11; i <= n; i++) {
+//     s *= i / 10;
+// }
+// console.log(s);
+
+// problem_93
+// let n = 5, s = 1;
+// for (let i = 11; i <= n; i++) {
+//     s *= (-1) ** 2 * i / 10;
+// }
+// console.log(s);
+
+// problem_94
+// let n = 2,
+//     a = 3;
+// console.log(a ** n);
+
+// problem_95
+// let n = 10,
+//     k = 2,
+//     s = 0;
+// for (let i = 1; i <= n; i++) {
+//     s += i ** k;
+// }
+// console.log(s);
+
+// problem_96
+// let a = 1,
+//     b = 5;
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+// problem_97
+let a = [];
+let b = [4, 5];
